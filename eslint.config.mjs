6849766edeb4da06ae53abb19311c26192363323
Harshavardhan Varma <harshasurya02@gmail.com>
2025-10-19 03:07:00 +0530

    feat: update ESLint configuration to warn on explicit 'any' type usage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,6 +25,7 @@ const eslintConfig = [
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
