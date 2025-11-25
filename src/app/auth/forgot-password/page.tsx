6999f5349489e6b8268de977f621e490dbdf9ff3
Harshavardhan Varma <harshasurya02@gmail.com>
2025-11-25 16:56:04 +0530

    feat: add forgot password page with form component

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 
 
 export default function ForgotPasswordPage() {
-  return <Suspense>
-    <ForgotPasswordForm />;
+  return <Suspense fallback={<div>Loading...</div>}>
+    <ForgotPasswordForm />
   </Suspense>
 }
