3ccb4dcb88ab003a064f7f1e0b362722a0a964ac
Harshavardhan Varma <harshasurya02@gmail.com>
2025-11-25 16:55:52 +0530

    feat: add forgot password page

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,6 +1,9 @@
 import { ForgotPasswordForm } from "@/components/forgot-password-form";
+import { Suspense } from "react";
 
 
 export default function ForgotPasswordPage() {
-  return <ForgotPasswordForm />;
+  return <Suspense>
+    <ForgotPasswordForm />;
+  </Suspense>
 }
