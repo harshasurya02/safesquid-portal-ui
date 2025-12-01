3dec5421bdcd825e942f3315c34fd17da66c58f0
Harshavardhan Varma <harshasurya02@gmail.com>
2025-12-01 17:26:33 +0530

    feat: add authentication layout component with navbar and content area

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -29,7 +29,7 @@ export default function AuthLayout({
         </div>
       </div> */}
 
-      <div className="flex flex-col items-center justify-center px-4 pb-4 min-h-screen md:min-h-auto">
+      <div className="flex flex-col items-center justify-center px-4 pb-4 min-h-screen md:min-h-auto md:pt-20">
         <div className="w-full max-w-[clamp(20rem,90vw,34.5rem)] p-[clamp(0.5rem,2vw,0rem)] my-auto">
           {children}
         </div>
