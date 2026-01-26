8dd787cb9bd366d6321b2164cf0de5d3156c076c
harshavardhanvarma02 <harshavardhanvarma02@gmail.com>
2026-01-26 23:07:55 +0530

    ...

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ const eslintConfig = [
       "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
     },
   },
 ];
